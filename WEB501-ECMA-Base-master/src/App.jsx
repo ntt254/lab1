c4b111b3946db13ae94902b5cdf4abfd97e0f853
Tu <tienvinh1611@gmail.com>
2025-12-03 15:09:26 +0700

    ok

--- WEB501-ECMA-Base-master/src/App.jsx
+++ WEB501-ECMA-Base-master/src/App.jsx
@@ -1,36 +1,42 @@
 import { Toaster } from "react-hot-toast";
-import { Link } from "react-router-dom";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+
 import AddPage from "./pages/Add";
 import EditPage from "./pages/Edit";
 import ListPage from "./pages/List";
 
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
   return (
     <>
+      {/* NAVBAR */}
       <nav className="bg-blue-600 text-white shadow">
         <div className="max-w-6xl mx-auto px-4 h-16 flex items-center justify-between">
-          <Link to="#" className="text-xl font-semibold">
+          <Link to="/" className="text-xl font-semibold">
             <strong>WEB501 App</strong>
           </Link>
 
+          {/* MENU */}
           <div className="hidden md:flex items-center space-x-8">
-            <Link to="#" className="hover:text-gray-200">
+            <Link to="/" className="hover:text-gray-200">
               Trang chủ
             </Link>
-            <Link to="./list" className="hover:text-gray-200">
+            <Link to="/list" className="hover:text-gray-200">
               Danh sách
             </Link>
-            <Link to="./add" className="hover:text-gray-200">
+            <Link to="/add" className="hover:text-gray-200">
               Thêm mới
             </Link>
           </div>
 
+          {/* AUTH */}
           <div className="hidden md:flex items-center space-x-6">
-            <Link to="#" className="hover:text-gray-200">
+            <Link to="/login" className="hover:text-gray-200">
               Đăng nhập
             </Link>
-            <Link to="#" className="hover:text-gray-200">
+            <Link to="/register" className="hover:text-gray-200">
               Đăng ký
             </Link>
           </div>
@@ -42,10 +48,18 @@ function App() {
         <h1 className="text-4xl font-bold mb-4">Chào mừng đến với WEB501</h1>
         <p className="text-lg text-gray-600">Ứng dụng quản lý dữ liệu</p>
       </div>
+
+      {/* ROUTES */}
       <Routes>
-        <Route path="/List" element={<ListPage />} />
-        <Route path="/Add" element={<AddPage />} />
-        <Route path="/Edit/:id" element={<EditPage />} />
+        <Route path="/list" element={<ListPage />} />
+        <Route path="/add" element={<AddPage />} />
+        <Route path="/edit/:id" element={<EditPage />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Trang Home */}
+        <Route path="/" element={<div></div>} />
       </Routes>
 
       <Toaster />
